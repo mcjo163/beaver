b3cf725d813c04cbba516dd507f39333826eb5f9
Micah Johnson <mj1262@messiah.edu>
2019-09-24 14:20:52 -0400

    updated login.php

--- login.php
+++ login.php
@@ -1,5 +1,11 @@
+<!DOCTYPE html>
+<head>
+    <title>Login submission</title>
+</head>
+<body>
 <?php
     $username = htmlspecialchars($_POST['username']);
     $password = htmlspecialchars($_POST['password']);
     echo "<p>username: $username <br>password: $password</p>";
 ?>
+</body>
